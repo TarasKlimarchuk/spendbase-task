f677cd73b38c86d822a51d3db1c11229a8576958
satomeister <klimarcuktaras@gmail.com>
2023-12-15 15:14:24 +0200

    configurate project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+};
+
+export default App;
